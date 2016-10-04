f0bf2f7d9e247ed15bb068cf954f8be6326af2dc
Mario Ezequiel Ginsberg <eginsberg@ws10.labo3>
2016-10-04 19:01:07 -0300

    enunciado y algo del ej1

---
+++ ej1/ej1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Arista {
+	int nodo;
+	int capacidad;
+};
+
+struct Nodo {
+	int tipo; // 0: est; 1: escuela; 2: nada
+	list<Arista> adyacentes;
+};
+
+int main() {
+	int N, M;
+	cin >> N >> M;
+
+	vector<Nodo> grafo;
+	
+	return 0;
+}
